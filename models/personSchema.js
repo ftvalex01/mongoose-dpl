67aa6683d7f424c1d9459e61ef484270fc82c509
ftvalex01 <ftvalex@hotmail.com>
2024-01-22 20:33:37 +0000

    ejercicio 2.1

--- models/personSchema.js
+++ models/personSchema.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const personSchema = new mongoose.Schema({
     name: {
       type: String,
@@ -7,8 +9,8 @@ const personSchema = new mongoose.Schema({
     favoriteFoods: [String]  // Un arreglo de strings
   });
   
+
 // Creación del modelo a partir del esquema
 const Person = mongoose.model('Person', personSchema);
-
 // Exportar el modelo si estás usando módulos (opcional)
 module.exports = Person;  
